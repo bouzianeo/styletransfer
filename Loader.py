76fede5e300eb6b29145ce1ecd63fda00fe9bac9
Bouziane Othmane <othmane.bouziane@student.ecp.fr>
2019-03-06 19:38:47 +0100

    fix loader size

--- Loader.py
+++ Loader.py
@@ -2,35 +2,44 @@ import torch
 from PIL import Image
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
+import os
 
 
-def image_loader(image_name):
+def image_loader(style_path, content_path):
+    style_path = os.path.join("images/style", style_path)
+    content_path = os.path.join("images/content", content_path)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    imsize = 1024 if torch.cuda.is_available() else 512
+    imsize = 1024 if torch.cuda.is_available() else 128
     loader = transforms.Compose([transforms.Resize(imsize), transforms.ToTensor()])
-    ratio = 9/16
-    image = Image.open(image_name)
-    w, h = image.size
+
+    content_image = Image.open(content_path)
+    w, h = content_image.size
+    ratio = w / h
+    style_image = Image.open(style_path)
+    w, h = style_image.size
     if w/h <= ratio:
         h = w/ratio
     else:
         w = h*ratio
-    image=image.crop((0, 0, w, h))
-    image = loader(image).unsqueeze(0)
-    return image.to(device, torch.float)
+    style_image = style_image.crop((0, 0, w, h))
+    style_image = loader(style_image).unsqueeze(0)
+    content_image = loader(content_image).unsqueeze(0)
+    return style_image.to(device, torch.float), content_image.to(device, torch.float)
 
-def imshow(tensor, title=None, save=False):
-    plt.figure()
-    unloader = transforms.ToPILImage()
-    image = tensor.cpu().clone()
-    image = image.squeeze(0)
-    image = unloader(image)
-    if save:
-        image.save("output.jpg", "JPEG", optimize=True)
-    plt.imshow(image)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)
+
+# def imshow(tensor, title=None, save=False):
+#     plt.figure()
+#     unloader = transforms.ToPILImage()
+#     image = tensor.cpu().clone()
+#     image = image.squeeze(0)
+#     image = unloader(image)
+#     if save:
+#         image.save("output.jpg", "JPEG", optimize=True)
+#     plt.imshow(image)
+#     if title is not None:
+#         plt.title(title)
+#     plt.pause(0.001)
+
 
 def imsave(tensor):
     unloader = transforms.ToPILImage()
