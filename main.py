699dbdf7f60b157881ada9dc6bd573bf4cb6a43d
Bouziane Othmane <othmane.bouziane@student.ecp.fr>
2019-03-24 12:38:47 +0100

    new weight

--- main.py
+++ main.py
@@ -16,33 +16,24 @@ if __name__ == "__main__":
     #         output = vggdream(content_img, num_steps=300, style_weight=1000000, content_weight=1)  # style_weight=100000, content_weight=10
     #         imsave(output, style[:-4] + "-" + content)
 
-    content = 'centraleext.jpg'
-    style = 'picasso.jpg'
-    style_img, content_img = image_loader(style, content)
-    print("Content image : {} and style image : {}".format(content, style))
-    style_img, content_img = image_loader(style, content)
 
-    vggdream = VGG16(style_img, content_img)
-
-    output = vggdream(content_img, num_steps=300, style_weight=1000000, content_weight=1)  # style_weight=100000, content_weight=10
-    imsave(output, style[:-4] + "-" + content)
 
     ########################################
     #               Grid Search            #
     ########################################
-    #
-    # content = 'centraleext.jpg'
-    # style = 'picasso.jpg'
-    # style_img, content_img = image_loader(style, content)
-    #
-    # assert style_img.size() == content_img.size(), \
-    #     "You have to to import style and content images of the same size"
-    #
-    # vggdream = VGG16(style_img, content_img)
-    # style_weights = {1, 10, 100, 1000, 10000, 100000}
-    # content_weights = {1, 10, 100, 1000, 10000, 100000}
-    # for style_weight in style_weights:
-    #     for content_weight in content_weights:
-    #         print("Content weigh : {} and style weight : {}".format(content_weight, style_weight))
-    #         output = vggdream(content_img, num_steps=300, style_weight=style_weight, content_weight=content_weight)
-    #         imsave(output, str(style_weight) + "_" + str(content_weight) + "_" + style)
+
+    content = 'centraleext.jpg'
+    style = 'picasso.jpg'
+    style_img, content_img = image_loader(style, content)
+
+    assert style_img.size() == content_img.size(), \
+        "You have to to import style and content images of the same size"
+
+    vggdream = VGG16(style_img, content_img)
+    style_weights = {50000, 500000, 1000000, 10000000, 100000000}
+    content_weights = {1}
+    for style_weight in style_weights:
+        for content_weight in content_weights:
+            print("Content weigh : {} and style weight : {}".format(content_weight, style_weight))
+            output = vggdream(content_img, num_steps=300, style_weight=style_weight, content_weight=content_weight)
+            imsave(output, str(style_weight) + "_" + str(content_weight) + "_" + style)
